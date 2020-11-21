c300426c89240a163e0f31b060c588373f365af7
VayerMaking <martin.vayer@gmail.com>
2020-11-21 20:06:34 +0200

    added saving password to a file

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ import config
 import hashlib
 import random
 import enquiries
+import json
+
+with open('passwords.txt') as j:
+    pass_dict = json.load(j)
 
 def add_password(pass_dict):
+    print("enter a site name")
+    new_site = str(input())
+    print("ender the password")
     new_pass = str(input())
     hashed_pass = hash_password(new_pass)
-    pass_dict[len(pass_dict)+1] = hashed_pass
+    #pass_dict[len(pass_dict)+1] = hashed_pass
+    pass_dict[new_site] = hashed_pass
     return pass_dict
 
 def hash_password(password):
     password += random.choice(wordlist)
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
+def save_new_passwords(pass_dict):
+    with open("passwords.txt", 'w', encoding = 'utf-8') as f:
+        f.write(json.dumps(pass_dict))
+        f.close()
+
+
+
 wordlist = ["apple", "qwerty", "asdf"]
-pass_dict = {"1" : "qwerty"}
 
 
 options = ['add a new password', 'get a password']
@@ -24,6 +38,7 @@ print("you chose:", choice)
 if choice == options[0]:
     print("adding a new password")
     add_password(pass_dict)
+    save_new_passwords(pass_dict)
 elif choice == options[1]:
     print("getting your password")
 
