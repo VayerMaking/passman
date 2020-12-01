d87936821e7fd636c6b34639818a803280ef83ce
VayerMaking <martin.vayer@gmail.com>
2020-12-01 17:05:45 +0200

    encrypt fix

--- passman.py
+++ passman.py
@@ -3,25 +3,62 @@ import random
 import enquiries
 import json
 import subprocess
-import os
+import scrypt, os, binascii
+from Crypto.Cipher import AES
+
+
+
+def encrypt_password(password, key):
+    print("pass", type(password))
+    password = password.encode('utf8')
+    print("pass", type(password))
+    kdfSalt = os.urandom(16)
+    secretKey = scrypt.hash(key, kdfSalt, N=16384, r=8, p=1, buflen=32)
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(password)
+    print((kdfSalt, ciphertext, aesCipher.nonce, authTag))
+    return (kdfSalt, ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_password(encrypted_password, key):
+    kdfSalt, ciphertext, nonce, authTag = encrypted_password
+    secretKey = scrypt.hash(key, kdfSalt, N=16384, r=8, p=1, buflen=32)
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
 
 
 def load_passwords():
-    with open('passwords.txt') as j:
-        pass_dict = json.load(j)
-    return pass_dict
+    try:
+        with open('passwords.txt') as j:
+            pass_dict = json.load(j)
+        return pass_dict
+    except json.decoder.JSONDecodeError:
+        pass_dict = {}
+        return pass_dict
+
 
 def add_password(pass_dict):
     print("enter a site name")
     new_site = str(input())
     print("ender the password")
     new_pass = str(input())
-    hashed_pass = hash_password(new_pass)
+    master_pass = get_master_password().encode('utf-8')
     #pass_dict[len(pass_dict)+1] = hashed_pass
-    pass_dict[new_site] = hashed_pass
+    #pass_dict[new_site] = str(hashed_pass)
+    hashed_pass = encrypt_password(new_pass, master_pass)
+    result =  (binascii.hexlify(hashed_pass[0]).decode('utf8'),binascii.hexlify(hashed_pass[1]).decode('utf8'),binascii.hexlify(hashed_pass[2]).decode('utf8'),binascii.hexlify(hashed_pass[3]).decode('utf8'))
+    #print("redsult", result)
+    #print(type(hashed_pass))
+    #for i in result:
+        #print(type(i))
+    #    i = i.decode('utf8')
+    #    print(i)
+    #print("redsult decoded", result)
+
+    pass_dict.update({new_site: result})
     return pass_dict
 
-def get_macine_id():
+def get_machine_id():
     process = subprocess.check_output(['cat', '/var/lib/dbus/machine-id'])
     result = str(process)
     result = result[:-3]
@@ -49,6 +86,7 @@ def set_machine_id():
         process = subprocess.Popen(['cat', '/var/lib/dbus/machine-id'], stdout=f)
 
 def save_new_passwords(pass_dict):
+    print(pass_dict)
     with open("passwords.txt", 'w', encoding = 'utf-8') as f:
         f.write(json.dumps(pass_dict))
         f.close()
@@ -59,7 +97,9 @@ def get_password(pass_dict):
     # add auth here with master password
     if auth() == True:
         try:
-            return pass_dict[site_name]
+            master_pass = get_master_password().encode('utf-8')
+            password = decrypt_password(pass_dict[site_name], master_pass)
+            return password
         except KeyError:
             print("There is no {} site in your passman!".format(site_name))
     else:
@@ -79,4 +119,5 @@ def get_master_password():
     with open('.master_pass', 'r') as f:
         master_pass = f.read().replace('\n', '')
         f.close()
+        print(type(master_pass))
     return master_pass
