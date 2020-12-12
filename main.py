dd21686ca32c187a58818e295a5199fe8960c22c
VayerMaking <martin.vayer@gmail.com>
2020-12-12 16:00:49 +0200

    more ui changes

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ else:
 os.system('clear')
 
 while True:
-    options = [ 'get a password', 'add a new password', 'set the machine_id(requires password)', 'create master password', 'get a password with menu', 'exit']
+    options = [ 'get a password', 'add a new password', 'get a password with menu', 'settings', 'exit']
+    settings_options = ['set the machine_id(requires password)', 'create master password', 'exit']
     choice = enquiries.choose('Choose one of these options: ', options)
     print("you chose:", choice)
 
@@ -27,16 +28,22 @@ while True:
         passman.add_password(pass_dict)
         passman.save_new_passwords(pass_dict)
     elif choice == options[2]:
-        print("setting machine_id...")
-        passman.set_machine_id()
-    elif choice == options[3]:
-        print("creating master password...")
-        passman.create_master_password()
-        passman.generate_key()
-    elif choice == options[4]:
         print("getting password...")
         print("your password is: ", passman.get_password_menu(pass_dict))
-    elif choice == options[5]:
+    elif choice == options[3]:
+        print("entering setting menu...")
+        settings_choice = enquiries.choose('Choose one of these options: ', settings_options)
+        if settings_choice == settings_options[0]:
+            print("setting machine_id...")
+            passman.set_machine_id()
+        elif settings_choice == settings_options[1]:
+            print("creating master password...")
+            passman.create_master_password()
+            passman.generate_key()
+        elif settings_choice == settings_options[2]:
+            print("exiting...")
+            quit()
+    elif choice == options[4]:
         print("exiting...")
         quit()
 
